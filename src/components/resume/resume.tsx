357764960fc2ca6ffd4cd84df5a970587d9839a4
Tayden Flitcroft <taydengoodeill@gmail.com>
2022-05-26 16:23:23 -0500

    accept blob from backend instead of url

--- src/components/resume/resume.tsx
+++ src/components/resume/resume.tsx
@@ -8,20 +8,16 @@ import {NavigationContext} from '../../providers/navigation'
 import '@react-pdf-viewer/core/lib/styles/index.css'
 import '@react-pdf-viewer/zoom/lib/styles/index.css'
 import I18N from '../I18N/i18n'
-import {PortfolioDataContext} from '../../providers/portfolio-data'
+import {BaseUrlContext} from '../../providers/base-url'
 import './resume.scss'
-import SuspenseLoader from '../suspense-loader/suspense-loader'
 
 const Resume = () => {
     const { setHasMounted } = useContext(NavigationContext)
-    const { pdfWorkerUrl, resumePdfUrl, fetchResumeData } = useContext(PortfolioDataContext)
+    const baseUrl = useContext(BaseUrlContext)
 
     useEffect(() => {
-        fetchResumeData()
-        if (pdfWorkerUrl && resumePdfUrl) {
-            setHasMounted(true)
-        }
-    }, [pdfWorkerUrl, resumePdfUrl])
+        setHasMounted(true)
+    }, [])
 
     const fullScreenPluginInstance = fullScreenPlugin({
         onEnterFullScreen: (zoom) => {
@@ -39,7 +35,6 @@ const Resume = () => {
     const { PrintButton } = printPluginInstance
     const { DownloadButton } = getFilePluginInstance
 
-    if (!pdfWorkerUrl || !resumePdfUrl) return <SuspenseLoader/>
 
     return (
         <div className="resume">
@@ -47,7 +42,7 @@ const Resume = () => {
             <I18N className="roll-down-3 uppercase" markdown name="resume.subHeader" />
             <I18N className="roll-down-4 uppercase" markdown name="resume.contactMe" />
             <div className="pdf-container roll-up-4">
-                <Worker workerUrl={ pdfWorkerUrl }>
+                <Worker workerUrl={ `${baseUrl}/asset?fileName=pdf-worker.min.js` }>
                     <div className="pdf-container__toolbar">
                         <div>
                             <EnterFullScreenButton />
@@ -61,7 +56,7 @@ const Resume = () => {
                             <DownloadButton />
                         </div>
                     </div>
-                    <Viewer fileUrl={ resumePdfUrl } plugins={[fullScreenPluginInstance, zoomPluginInstance, printPluginInstance, getFilePluginInstance]} />
+                    <Viewer fileUrl={ `${baseUrl}/asset?fileName=resume.pdf` } plugins={[fullScreenPluginInstance, zoomPluginInstance, printPluginInstance, getFilePluginInstance]} />
                 </Worker>
             </div>
         </div>
--- src/providers/portfolio-data.tsx
+++ src/providers/portfolio-data.tsx
@@ -8,28 +8,35 @@ PortfolioDataContext.displayName = 'PortfolioData'
 const PorfolioDataProvider = ({ children }) => {
     const baseUrl = useContext(BaseUrlContext)
     const [projectData, setProjectData] = useState<ProjectDataInterface>({})
-    const [pdfWorkerUrl, setPdfWorkerUrl] = useState<string>('')
-    const [resumePdfUrl, setResumePdfUrl] = useState<string>('')
+    // const [pdfWorkerUrl, setPdfWorkerUrl] = useState<string>('')
+    // const [resumePdfUrl, setResumePdfUrl] = useState<any>('')
 
     useEffect(() => {
         fetch(`${baseUrl}/projects`)
             .then(res => res.json())
             .then(res => setProjectData(res))
             .catch(() => {})
+        // fetch(`${baseUrl}/asset?fileName=resume.pdf`)
+        //     .then(res => res.json())
+        //     .then(({ url }) => {
+        //         fetch(url)
+        //             .then(res => res.blob())
+        //             .then(res => setResumePdfUrl(URL.createObjectURL(res)))
+        //             .catch(() => {})
+        //     })
+        //     .catch(() => {})
+        // fetch(`${baseUrl}/asset?fileName=pdf-worker.min.js`)
+        //     .then(res => res.json())
+        //     .then(({ url }) => {
+        //         fetch(url)
+        //             .then(res => res.blob())
+        //             .then(res => setPdfWorkerUrl(URL.createObjectURL(res)))
+        //             .catch(() => {})
+        //     })
+        //     .catch(() => {})
     }, [])
 
-    const fetchResumeData = () => {
-        fetch(`${baseUrl}/asset?fileName=resume.pdf`)
-            .then(res => res.json())
-            .then(({ url }) => setResumePdfUrl(url))
-            .catch(() => {})
-        fetch(`${baseUrl}/asset?fileName=pdf-worker.min.js`)
-            .then(res => res.json())
-            .then(({ url }) => setPdfWorkerUrl(url))
-            .catch(() => {})
-    }
-
-    const portfolioData = { projectData, pdfWorkerUrl, resumePdfUrl, fetchResumeData }
+    const portfolioData = { projectData }
 
     return (
         <PortfolioDataContext.Provider value={ portfolioData }>
